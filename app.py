b35fabc5f6aacf53b0447d47fb05746e265eacd8
Matias Melo Estudio <matias.melo.estudio@gmail.com>
2023-08-25 00:01:43 -0400

    Llevando programa barbara python en Github

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+dato_guardado = None
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/guardar', methods=['POST'])
+def guardar():
+    global dato_guardado
+    dato = request.form.get('dato')
+    dato_guardado = dato
+    return "Dato guardado: " + dato
+
+@app.route('/obtener_dato', methods=['GET'])
+def obtener_dato():
+    global dato_guardado
+    return jsonify({'dato': dato_guardado})
+
+if __name__ == '__main__':
+    app.run(debug=True)
